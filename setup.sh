354145d817c9b37077fc6fce9a99a5dad3e0ccb5
Hao Chen <chenh1024@gmail.com>
2016-07-23 15:15:39 -0700

    add oh-my-zsh as a submodule

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
-ln -s ~/dotfiles/zsh/zshrc ~/.zshrc
-ln -s ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
+base_dir=$HOME/dotfiles
+
+ln -s $base_dir/zsh/oh-my-zsh ~/.oh-my-zsh
+ln -s $base_dir/zsh/zshrc ~/.zshrc
+ln -s $base_dir/tmux/tmux.conf ~/.tmux.conf
 

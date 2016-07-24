f7a29cfef46bf528fb0f9e95a3c2c63fd26482c8
Hao Chen <chenh1024@gmail.com>
2016-07-23 19:19:11 -0700

    add basic vimrc

--- setup.sh
+++ setup.sh
@@ -2,5 +2,8 @@ base_dir=$HOME/dotfiles
 
 ln -s $base_dir/zsh/oh-my-zsh ~/.oh-my-zsh
 ln -s $base_dir/zsh/zshrc ~/.zshrc
+
 ln -s $base_dir/tmux/tmux.conf ~/.tmux.conf
+
+ln -s $base_dir/vim/vimrc ~/.vimrc
 

5ca3232029587d34fcab1fc1c02258a4fde5c4bf
Hao Chen <chenh1024@gmail.com>
2016-11-21 22:20:25 -0800

    [hs] prefix + w to choose window with hints

--- hammerspoon/window.lua
+++ hammerspoon/window.lua
@@ -3,3 +3,6 @@ local prefix = require("prefix")
 hs.grid.setGrid('6x4', nil, nil)
 hs.grid.setMargins({0, 0})
 prefix.bind('', 'g', function() hs.grid.show() end)
+
+hs.hints.hintCharacters = 'asdfgqwertzxcvb12345'
+prefix.bind('', 'w', function() hs.hints.windowHints() end)

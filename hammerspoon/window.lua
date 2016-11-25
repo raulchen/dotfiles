53b9b728c9b98050ccf12a4c646e9eeb0599ec3a
Hao Chen <chenh1024@gmail.com>
2016-11-25 12:51:50 -0800

    [hs] option+[shift+]tab to switch window

--- hammerspoon/window.lua
+++ hammerspoon/window.lua
@@ -7,3 +7,25 @@ prefix.bind('', 'g', function() hs.grid.show() end)
 
 hs.hints.hintChars = utils.strToTable('ASDFGQWERTZXCVB12345')
 prefix.bind('', 'w', function() hs.hints.windowHints() end)
+
+local switcher = hs.window.switcher.new(nil, {
+    fontName = ".AppleSystemUIFont",
+    textSize = 16,
+    textColor = { white = 0, alpha = 1 },
+    highlightColor = { white = 0.6, alpha = 0.9 },
+    backgroundColor = { white = 1, alpha = 0.8 },
+    titleBackgroundColor = { white = 0.9, alpha = 0.9 },
+    showThumbnails = false,
+    showSelectedThumbnail = false,
+})
+
+local function nextWindow()
+    switcher:next()
+end
+
+local function previousWindow()
+    switcher:previous()
+end
+
+hs.hotkey.bind('alt', 'tab', nextWindow, nil, nextWindow)
+hs.hotkey.bind('alt-shift', 'tab', previousWindow, nil, previousWindow)

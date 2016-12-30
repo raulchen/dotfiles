bc303d61ab50a7060795206b0f7750c4ceaa39d3
raulchen <me@hchen.me>
2016-12-30 13:32:18 +0800

    [hs] misc changes on prefix mode

--- hammerspoon/prefix.lua
+++ hammerspoon/prefix.lua
@@ -1,36 +1,43 @@
--- Simulate tmux's key binding: prefix + hotkey
+-- Tmux style hotkey binding: prefix + hotkey
 
 local module = {}
 
-local TIMEOUT = 3
+local TIMEOUT = 5
 
 local modal = hs.hotkey.modal.new('ctrl', 'space')
 
 function modal:entered()
-    modal.alertId = hs.alert.show("Prefix mode", TIMEOUT)
-    hs.timer.doAfter(TIMEOUT, function() modal:exit() end)
+    modal.alertId = hs.alert.show("Prefix Mode", 9999)
+    modal.timer = hs.timer.doAfter(TIMEOUT, function() modal:exit() end)
 end
 
 function modal:exited()
     if modal.alertId then
         hs.alert.closeSpecific(modal.alertId)
     end
+    module.cancelTimeout()
 end
 
-modal:bind('', 'escape', function() modal:exit() end)
-modal:bind('ctrl', 'space', function() modal:exit() end)
+function module.exit()
+    modal:exit()
+end
 
-function module.bind(mod, key, fn, autoExit)
-    if autoExit == nil or autoExit then
-        bindFn = function()
-            modal:exit()
-            fn()
-        end
-    else
-        bindFn = fn
+function module.cancelTimeout()
+    if modal.timer then
+        modal.timer:stop()
     end
-    modal:bind(mod, key, bindFn)
 end
+
+function module.bind(mod, key, fn)
+    modal:bind(mod, key, nil, function() fn(); module.exit() end)
+end
+
+function module.bindMultiple(mod, key, pressedFn, releasedFn, repeatFn)
+    modal:bind(mod, key, pressedFn, releasedFn, repeatFn)
+end
+
+module.bind('', 'escape', module.exit)
+module.bind('ctrl', 'space', module.exit)
 
 module.bind('', 'd', hs.toggleConsole)
 module.bind('', 'r', hs.reload)

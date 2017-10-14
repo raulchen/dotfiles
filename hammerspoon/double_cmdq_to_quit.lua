37a9cb4d2f856eb11ae70247819a5c85a54c97bd
Hao Chen <chenh1024@gmail.com>
2017-10-14 11:11:19 -0700

    [hs][double_cmdq_to_quit] support whitelist

--- hammerspoon/double_cmdq_to_quit.lua
+++ hammerspoon/double_cmdq_to_quit.lua
@@ -2,13 +2,26 @@
 
 local quitModal = hs.hotkey.modal.new('cmd','q')
 
+-- For special apps that have multiple menu items starting with "Quit",
+-- match the full name
+local WHITELIST = {}
+WHITELIST["com.tencent.QQMusicMac"] = "退出QQ音乐"
+
 function quitModal:entered()
     hs.alert.show("Press Cmd+Q again to quit", 1)
     hs.timer.doAfter(1, function() quitModal:exit() end)
 end
 
 local function doQuit()
-    local res = hs.application.frontmostApplication():selectMenuItem("^(Quit|退出).*$")
+    local app = hs.application.frontmostApplication()
+    local bundleID = app:bundleID()
+    local menu =  "(Quit|退出).*"
+    local regex = true
+    if WHITELIST[bundleID] then
+        menu = WHITELIST[bundleID]
+        regex = false
+    end
+    app:selectMenuItem(menu, regex)
     quitModal:exit()
 end
 

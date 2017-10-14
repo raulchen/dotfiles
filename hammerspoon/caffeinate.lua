85bcd5cbfab5f2aaec2b56c1246c823da9db769d
Hao Chen <chenh1024@gmail.com>
2017-10-14 12:13:27 -0700

    [hs][caffeinate] change sleep type to displayIdle

--- hammerspoon/caffeinate.lua
+++ hammerspoon/caffeinate.lua
@@ -5,9 +5,12 @@ local prefix = require('prefix')
 local menu = nil
 
 local function toggle()
-    local enabled = hs.caffeinate.toggle('system')
+    local enabled = hs.caffeinate.toggle('displayIdle')
     if enabled then
         menu = hs.menubar.new():setTitle('☕')
+        menu:setMenu({
+            { title = 'Turn off Caffeinate', fn = toggle }
+        })
     else
         menu:delete()
     end

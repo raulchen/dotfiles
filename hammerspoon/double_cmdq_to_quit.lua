efd6a93196c6905fa36b9dd6a266bd83804e1d6e
Hao Chen <chenh1024@gmail.com>
2017-11-25 12:02:40 -0800

    [hs][double_cmd_to_quit] use 'kill' api, instead of matching menu item

--- hammerspoon/double_cmdq_to_quit.lua
+++ hammerspoon/double_cmdq_to_quit.lua
@@ -2,11 +2,6 @@
 
 local quitModal = hs.hotkey.modal.new('cmd','q')
 
--- For special apps that have multiple menu items starting with "Quit",
--- match the full name
-local WHITELIST = {}
-WHITELIST["com.tencent.QQMusicMac"] = "退出QQ音乐"
-
 function quitModal:entered()
     hs.alert.show("Press Cmd+Q again to quit", 1)
     hs.timer.doAfter(1, function() quitModal:exit() end)
@@ -14,15 +9,7 @@ end
 
 local function doQuit()
     local app = hs.application.frontmostApplication()
-    local bundleID = app:bundleID()
-    local menu =  "(Quit|退出).*"
-    local regex = true
-    if WHITELIST[bundleID] then
-        menu = WHITELIST[bundleID]
-        regex = false
-    end
-    app:selectMenuItem(menu, regex)
-    quitModal:exit()
+    app:kill()
 end
 
 quitModal:bind('cmd', 'q', doQuit)

dc583fc39be858e24fb4c02cbc872917c52d40a4
Hao Chen <chenh1024@gmail.com>
2020-06-30 16:00:06 +0800

    rg searches hidden files

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -65,7 +65,7 @@ fz() {
 frg() {
     local _size=`echo "($LINES*0.35-2)/1" | bc`
     local _cmd="pcat %s -c %d -l $_size -n --color=always"
-    rg --color always $@ | fzf --ansi --reverse --no-sort \
+    rg --color always --hidden $@ | fzf --ansi --reverse --no-sort \
         --preview-window down:35% \
         --preview "echo {} |
                    awk -F':' '{printf \"$_cmd\",\$1,\$2}' |

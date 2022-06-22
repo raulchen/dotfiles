2430d4114482c72adf156515dae49532c9bd5493
Hao Chen <chenh1024@gmail.com>
2022-06-22 11:31:58 +0800

    [setup.sh] change tabs to spaces

--- setup.sh
+++ setup.sh
@@ -7,50 +7,50 @@ mkdir -p "$backup_dir"
 
 link_file() {
     local src=$1 dst=$2
-	local backup_dst=false delete_dst=false link_dst=true
+    local backup_dst=false delete_dst=false link_dst=true
     if [[ -e $dst ]]; then
-		current_link=$(readlink "$dst")
-		if [[ "$current_link" != "$src" ]]; then
-			while true; do
-				printf "File already exists: %s. What do you want?\n" "$dst"
-				printf "[r]eplace; [b]ack up; [s]kip: "
-				read -r op
-				case $op in
-					r )
-						delete_dst=true
-						link_dst=true
-						break;;
-					b )
-						backup_dst=true
-						delete_dst=true
-						link_dst=true
-						break;;
-					s )
-						link_dst=false
-						break;;
-					* )
-					   echo "Unrecognized option: $op";;
-			   esac
-			done
-		else
+        current_link=$(readlink "$dst")
+        if [[ "$current_link" != "$src" ]]; then
+            while true; do
+                printf "File already exists: %s. What do you want?\n" "$dst"
+                printf "[r]eplace; [b]ack up; [s]kip: "
+                read -r op
+                case $op in
+                    r )
+                        delete_dst=true
+                        link_dst=true
+                        break;;
+                    b )
+                        backup_dst=true
+                        delete_dst=true
+                        link_dst=true
+                        break;;
+                    s )
+                        link_dst=false
+                        break;;
+                    * )
+                       echo "Unrecognized option: $op";;
+               esac
+            done
+        else
             echo "$dst is already linked to $src"
-			link_dst=false
-		fi
+            link_dst=false
+        fi
     fi
-	if [[ "$backup_dst" == "true" ]]; then
-		local backup_file
+    if [[ "$backup_dst" == "true" ]]; then
+        local backup_file
         backup_file="$backup_prefix$(basename "$dst")"
-		mv "$dst" "$backup_file"
-		echo "$dst was backed up to $backup_file"
-	fi
-	if [[ "$delete_dst" == "true" ]]; then
-		rm -rf "$dst"
-	fi
-	if [[ "$link_dst" == "true" ]]; then
-		ln -s "$src" "$dst"
-		return 0
-	fi
-	return 1
+        mv "$dst" "$backup_file"
+        echo "$dst was backed up to $backup_file"
+    fi
+    if [[ "$delete_dst" == "true" ]]; then
+        rm -rf "$dst"
+    fi
+    if [[ "$link_dst" == "true" ]]; then
+        ln -s "$src" "$dst"
+        return 0
+    fi
+    return 1
 }
 
 echo "Setting up zsh..."
@@ -59,7 +59,7 @@ link_file "$base_dir/zsh/zshrc" ~/.zshrc
 
 echo "Setting up vim..."
 if link_file "$base_dir/vim/vimrc" ~/.vimrc ; then
-	vim +PlugInstall +qall
+    vim +PlugInstall +qall
 fi
 
 echo "Setting up tmux..."

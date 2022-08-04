e5a363762cea97a7218f2442e2d91e1fe6ca0847
Hao Chen <chenh1024@gmail.com>
2022-08-04 13:42:30 +0800

    [docker] allow pass arguments to build.sh

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,4 @@ set -e
 git_user=$(git config user.name)
 git_email=$(git config user.email)
 docker build --build-arg git_user="$git_user" --build-arg git_email="git_email" \
-    --platform linux/x86_64 -t my/dev .
+    --platform linux/x86_64 -t my/dev $@ .

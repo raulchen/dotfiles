0b439c0c7b77ca31826508e3d6f3a062315fa54d
Hao Chen <chenh1024@gmail.com>
2022-09-01 18:23:21 +0800

    [zsh] use vim wrapper instead of alias for nvim

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -12,11 +12,6 @@ alias d="dirs -v | head -10"
 alias vh="vim -c ':History'"
 alias tat="tmux a || tmux"
 
-if which nvim >/dev/null 2>&1; then
-    alias vim=nvim
-    alias vimdiff='nvim -d'
-fi
-
 alias proxy_on="export {http,https,ftp}_proxy=http://127.0.0.1:7890; export socks_proxy=socks5://127.0.0.1:7891; no_proxy='localhost,127.0.0.1,*.local'"
 alias proxy_off="unset {http,https,ftp,all,socks,no}_proxy"
 

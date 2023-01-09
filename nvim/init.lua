dabef875908b448c96fb2c4b4c9688e1e2a1338c
Hao Chen <chenh1024@gmail.com>
2023-01-09 11:56:21 +0800

    [neovim] Fix cmp_nvim_lsp config

--- nvim/init.lua
+++ nvim/init.lua
@@ -40,7 +40,7 @@ end
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 local servers = {
   'pyright',

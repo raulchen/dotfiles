1b0233ae8860bbd9a56f90aaee15081a0a7cf9f2
Hao Chen <chenh1024@gmail.com>
2023-02-14 15:52:58 +0800

    [nvim][dap] change C++ debugger to codelldb

--- nvim/init.lua
+++ nvim/init.lua
@@ -113,45 +113,26 @@ cmp.setup {
 }
 
 local dap = require('dap')
-local lldb_vscode = vim.fn.system('which lldb-vscode'):gsub('\n', '')
-if not string.find(lldb_vscode, "not found") then
-  dap.adapters.lldb = {
-      type = 'executable',
-      command = lldb_vscode, -- adjust as needed, must be absolute path
-      name = 'lldb'
-  }
-
-  dap.configurations.cpp = {
-      {
-          name = 'Launch',
-          type = 'lldb',
-          request = 'launch',
-          program = function()
-            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
-          end,
-          cwd = '${workspaceFolder}',
-          stopOnEntry = false,
-          args = {},
-
-          -- 💀
-          -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
-          --
-          --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
-          --
-          -- Otherwise you might get the following error:
-          --
-          --    Error on launch: Failed to attach to the target process
-          --
-          -- But you should be aware of the implications:
-          -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
-          -- runInTerminal = false,
-      },
-  }
-
-  -- If you want to use this for Rust and C, add something like this:
-  dap.configurations.c = dap.configurations.cpp
-  dap.configurations.rust = dap.configurations.cpp
-end
+dap.adapters.codelldb = {
+    type = 'server',
+    port = "${port}",
+    executable = {
+        command = 'codelldb',
+        args = { "--port", "${port}" },
+    }
+}
+dap.configurations.cpp = {
+    {
+        name = "Launch file",
+        type = "codelldb",
+        request = "launch",
+        program = function()
+          return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopOnEntry = false,
+    },
+}
 
 local dapui = require("dapui")
 dapui.setup()

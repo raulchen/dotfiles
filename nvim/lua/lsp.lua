fb13d4ae7e52c98e27f219612e84d08dd25152a6
Hao Chen <chenh1024@gmail.com>
2023-04-22 13:44:07 -0700

    [nvim] Add null-ls plugin and add black formatter for Python

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -112,3 +112,12 @@ cmp.setup {
         { name = 'luasnip' },
     },
 }
+
+-- === null-ls.nvim ===
+local null_ls = require("null-ls")
+
+null_ls.setup({
+    sources = {
+        null_ls.builtins.formatting.black,
+    },
+})

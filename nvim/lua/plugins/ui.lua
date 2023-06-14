7fb17ba48f3595c736adbdc366bb39984e651c30
Hao Chen <chenh1024@gmail.com>
2023-06-14 12:07:45 -0700

    [nvim] move navic component to status bar
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -58,13 +58,8 @@ return {
             "filename",
             path = 1,
           },
-        },
-      },
-      winbar = {
-        lualine_c = {
           {
-            function() return require("nvim-navic").get_location() end,
-            cond = function() return package.loaded["nvim-navic"] and require("nvim-navic").is_available() end,
+            "navic",
           },
         },
       },

07db0ba803daa9bbf58a9790c67ca90c6b2ec9db
Hao Chen <chenh1024@gmail.com>
2023-07-09 17:17:10 -0700

    [nvim] add friendly-snippets

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -44,6 +44,10 @@ end
 return {
   {
     'L3MON4D3/LuaSnip',
+    dependencies = { 'rafamadriz/friendly-snippets' },
+    config = function()
+      require("luasnip.loaders.from_vscode").lazy_load()
+    end,
   },
   {
     'saadparwaiz1/cmp_luasnip',

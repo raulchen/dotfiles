cfbef1dca58055ea1008615655be926e8b7e0ba5
Hao Chen <chenh1024@gmail.com>
2024-03-11 14:24:58 -0700

    [nvim] limit treesitter-context to 5 lines
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -92,6 +92,9 @@ return {
   {
     "nvim-treesitter/nvim-treesitter-context",
     config = function()
+      require("treesitter-context").setup{
+        multiline_threshold = 5,
+      }
       vim.cmd([[
         hi TreesitterContextBottom gui=underline guisp=Grey
         hi TreesitterContextLineNumberBottom gui=underline guisp=Grey

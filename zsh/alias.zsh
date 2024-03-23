6959b2744e8689ed8cf3cb9ab3cf9685d7504b5d
Hao Chen <chenh1024@gmail.com>
2024-03-23 11:44:00 -0700

    [zsh][tmux] make tat command attach to 'default' session

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -11,7 +11,7 @@ unalias d >/dev/null 2>&1
 alias d="dirs -v | head -10"
 alias vh="vim -c ':History'"
 alias vf="vim -c ':execute FilesOrGFiles()'"
-alias tat="tmux a || tmux"
+alias tat="tmux new -A -s default"
 
 alias proxy_on="export {http,https,ftp}_proxy=http://127.0.0.1:7890; export socks_proxy=socks5://127.0.0.1:7891; no_proxy='localhost,127.0.0.1,*.local'"
 alias proxy_off="unset {http,https,ftp,all,socks,no}_proxy"

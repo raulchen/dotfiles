575e83685e0c5e59e7d422ef4576f60e21759606
Hao Chen <chenh1024@gmail.com>
2024-04-19 16:41:12 -0700

    [zsh] set fzf ctrl-t and ctrl-r options
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

---
+++ zsh/frg.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# 1. Search for text in files using Ripgrep
+# 2. Interactively restart Ripgrep with reload action
+# 3. Open the file in Vim
+RG_PREFIX="rg --column --line-number --no-heading --color=always --smart-case "
+INITIAL_QUERY="${*:-}"
+: | fzf --ansi --disabled --query "$INITIAL_QUERY" \
+    --bind "start:reload:$RG_PREFIX {q}" \
+    --bind "change:reload:sleep 0.1; $RG_PREFIX {q} || true" \
+    --delimiter : \
+    --preview 'bat --color=always {1} --highlight-line {2}' \
+    --preview-window 'up,60%,border-bottom,+{2}+3/3,~3' \
+    --bind 'enter:become(vim {1} +{2})'

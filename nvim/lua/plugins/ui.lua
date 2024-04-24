8401e29b53340a66636464c2609b62767cdedebc
Hao Chen <chenh1024@gmail.com>
2024-04-23 21:02:08 -0700

    [nvim] move onedarkpro setup to a separate function

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -51,6 +51,56 @@ local function setup_neozoom()
   vim.keymap.set('n', '<leader>wz', function() vim.cmd('NeoZoomToggle') end, { desc = "Toggle zoom" })
 end
 
+local function setup_onedarkpro(_, _)
+    local color = require("onedarkpro.helpers")
+    local colors = {
+      red = '#ff5c57',
+      green = '#5af78e',
+      yellow = '#f3f99d',
+      blue = '#57c7ff',
+      magenta = '#ff6ac1',
+      cyan = '#9aedfe',
+      purple = color.lighten("purple", 15, "onedark"),
+      orange = color.brighten("orange", 15, "onedark"),
+      comment = color.lighten("comment", 5, "onedark"),
+      fg = color.brighten("fg", 5, "onedark"),
+      light_red = color.lighten("red", 15, "onedark"),
+    }
+
+    local highlights = {
+      Identifier = { fg = "${cyan}", extend = true },
+      ["@variable"] = { link = "Identifier" },
+      ["@variable.parameter"] = { link = "Identifier" },
+      ["@variable.member"] = { link = "Identifier" },
+      ["@odp.interpolation.python"] = { link = "Identifier" }, -- Variables in f-strings.
+      ["@property"] = { fg = "${cyan}", extend = true },
+      String = { fg = "${yellow}", extend = true },
+      pythonString = { link = "String", extend = true },
+      Character = { fg = "${yellow}", extend = true },
+      ["@string"] = { fg = "${yellow}", extend = true },
+      Constant = { fg = "${green}", extend = true },
+      ["@constant"] = { fg = "${green}", extend = true },
+      ["@constant.builtin"] = { link = "Constant" },
+      SpellBad = { undercurl = true, sp = "${red}" },
+      DiagnosticUnderlineError = { undercurl = true, sp = "${red}" },
+    }
+
+    local styles = {
+      comments = "italic",
+    }
+
+    require("onedarkpro").setup({
+      colors = colors,
+      highlights = highlights,
+      styles = styles,
+      options = {
+        cursorline = true,
+        highlight_inactive_windows = false,
+      }
+    })
+    vim.cmd [[colorscheme onedark]]
+end
+
 return {
   {
     "nvim-tree/nvim-tree.lua",
@@ -125,54 +175,6 @@ return {
   {
     "olimorris/onedarkpro.nvim",
     priority = 1000,
-    config = function(_, _)
-      local color = require("onedarkpro.helpers")
-      local colors = {
-        red = '#ff5c57',
-        green = '#5af78e',
-        yellow = '#f3f99d',
-        blue = '#57c7ff',
-        magenta = '#ff6ac1',
-        cyan = '#9aedfe',
-        purple = color.lighten("purple", 15, "onedark"),
-        orange = color.brighten("orange", 15, "onedark"),
-        comment = color.lighten("comment", 5, "onedark"),
-        fg = color.brighten("fg", 5, "onedark"),
-        light_red = color.lighten("red", 15, "onedark"),
-      }
-
-      local highlights = {
-        Identifier = { fg = "${cyan}", extend = true },
-        ["@variable"] = { link = "Identifier" },
-        ["@variable.parameter"] = { link = "Identifier" },
-        ["@variable.member"] = { link = "Identifier" },
-        ["@odp.interpolation.python"] = { link = "Identifier" }, -- Variables in f-strings.
-        ["@property"] = { fg = "${cyan}", extend = true },
-        String = { fg = "${yellow}", extend = true },
-        pythonString = { link = "String", extend = true },
-        Character = { fg = "${yellow}", extend = true },
-        ["@string"] = { fg = "${yellow}", extend = true },
-        Constant = { fg = "${green}", extend = true },
-        ["@constant"] = { fg = "${green}", extend = true },
-        ["@constant.builtin"] = { link = "Constant" },
-        SpellBad = { undercurl = true, sp = "${red}" },
-        DiagnosticUnderlineError = { undercurl = true, sp = "${red}" },
-      }
-
-      local styles = {
-        comments = "italic",
-      }
-
-      require("onedarkpro").setup({
-        colors = colors,
-        highlights = highlights,
-        styles = styles,
-        options = {
-          cursorline = true,
-          highlight_inactive_windows = false,
-        }
-      })
-      vim.cmd [[colorscheme onedark]]
-    end,
+    config = setup_onedarkpro,
   },
 }

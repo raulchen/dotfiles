4e8fdf872dea1f82e699f9158594fbf97d1225c8
Hao Chen <chenh1024@gmail.com>
2024-04-27 21:22:18 -0700

    [nvim] Add icons to completion menu items and fix LuaSnip

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -1,6 +1,40 @@
 local function setup_cmp(_, _)
   local luasnip = require('luasnip')
   local cmp = require('cmp')
+  local kind_icons = {
+    Text = "",
+    Method = "󰆧",
+    Function = "󰊕",
+    Constructor = "",
+    Field = "󰇽",
+    Variable = "󰂡",
+    Class = "󰠱",
+    Interface = "",
+    Module = "",
+    Property = "󰜢",
+    Unit = "",
+    Value = "󰎠",
+    Enum = "",
+    Keyword = "󰌋",
+    Snippet = "",
+    Color = "󰏘",
+    File = "󰈙",
+    Reference = "",
+    Folder = "󰉋",
+    EnumMember = "",
+    Constant = "󰏿",
+    Struct = "",
+    Event = "",
+    Operator = "󰆕",
+    TypeParameter = "󰅲",
+  }
+  local source_display_names = {
+    nvim_lsp = "LSP",
+    buffer = "Buffer",
+    path = "Path",
+    luasnip = "LuaSnip",
+    spell = "Spell",
+  }
   cmp.setup({
     snippet = {
       expand = function(args)
@@ -22,6 +56,13 @@ local function setup_cmp(_, _)
       { name = 'luasnip' },
       { name = 'spell' },
     },
+    formatting = {
+      format = function(entry, vim_item)
+        vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind)
+        vim_item.menu = string.format('[%s]', source_display_names[entry.source.name])
+        return vim_item
+      end
+    },
   })
 end
 
@@ -35,11 +76,13 @@ return {
         "hrsh7th/cmp-buffer", -- source for text in buffer
         "hrsh7th/cmp-path",   -- source for file system paths
         'f3fora/cmp-spell',
-        'L3MON4D3/LuaSnip',
-        dependencies = { 'rafamadriz/friendly-snippets' },
-        config = function()
-          require("luasnip.loaders.from_vscode").lazy_load()
-        end,
+        {
+          'L3MON4D3/LuaSnip',
+          dependencies = { 'rafamadriz/friendly-snippets' },
+          config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+          end,
+        },
       },
       'saadparwaiz1/cmp_luasnip',
     }

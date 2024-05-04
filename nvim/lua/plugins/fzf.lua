9dc506d3261f16f307caf925306ba3a9a52afd1f
Hao Chen <chenh1024@gmail.com>
2024-05-03 21:33:30 -0700

    [nvim] more fzf key mappings

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,11 +1,11 @@
 local opts = {
-  'fzf-native',
+  "fzf-native",
   winopts = {
     height = 0.9,
     width = 0.9,
     preview = {
-      vertical = 'down:50%',
-      horizontal = 'right:50%',
+      vertical = "down:50%",
+      horizontal = "right:50%",
       flip_columns = 140,
     },
   },
@@ -22,31 +22,45 @@ local opts = {
 }
 
 local function setup_fzf_lua()
-  local fzf_lua = require('fzf-lua')
+  local fzf_lua = require("fzf-lua")
   fzf_lua.setup(opts)
 
   local function keymap(lhs, rhs, desc, mode)
-    mode = mode or 'n'
+    mode = mode or "n"
     vim.keymap.set(mode, lhs, rhs, { desc = desc })
   end
 
-  keymap('<leader>fa', fzf_lua.builtin, 'All Fzf commands')
+  keymap("<leader>fa", fzf_lua.builtin, "Search all Fzf commands")
+  keymap("<leader>fr", fzf_lua.resume, "Resume last Fzf command")
   -- Buffers and files.
-  keymap('<leader>ff', fzf_lua.files, 'Find files')
-  keymap('<leader>fd', function() fzf_lua.files({ cwd = vim.fn.expand('%:p:h') }) end, 'Find files in current directory')
-  keymap('<leader>fh', fzf_lua.oldfiles, 'Find file history')
-  keymap('<leader>fb', fzf_lua.buffers, 'Find buffers')
+  keymap("<leader>ff", fzf_lua.files, "Find files")
+  keymap("<leader>fd", function() fzf_lua.files({ cwd = vim.fn.expand("%:p:h") }) end, "Find files in current directory")
+  keymap("<leader>fh", fzf_lua.oldfiles, "Find file history")
+  keymap("<leader>fb", fzf_lua.buffers, "Find buffers")
   -- Search
-  keymap('<leader>fs', fzf_lua.grep_cword, 'Searh word under cursor')
-  keymap('<leader>fs', fzf_lua.grep_visual, 'Searh visual selection', 'v')
+  keymap("<leader>fs", fzf_lua.grep, "Searh")
+  keymap("<leader>fs", fzf_lua.grep_visual, "Searh visual selection", "v")
+  keymap("<leader>fS", fzf_lua.grep_cword, "Searh word under cursor")
   -- Tags
-  keymap('<leader>ft', fzf_lua.btags, 'Find tags in current buffer')
+  keymap("<leader>ft", fzf_lua.btags, "Find tags in current buffer")
   -- Misc
-  keymap('<leader>f:', fzf_lua.command_history, 'Find command history')
-  keymap('<leader>f/', fzf_lua.search_history, 'Find search history')
-  keymap('<leader>f"', fzf_lua.registers, 'Find registers')
-  keymap('<leader>f\'', fzf_lua.marks, 'Find marks')
-  keymap('<leader>fj', fzf_lua.jumps, 'Find jump locations')
+  keymap("<leader>f:", fzf_lua.command_history, "Find command history")
+  keymap("<leader>f/", fzf_lua.search_history, "Find search history")
+  keymap("<leader>f\"", fzf_lua.registers, "Find registers")
+  keymap("<leader>f'", fzf_lua.marks, "Find marks")
+  keymap("<leader>fj", fzf_lua.jumps, "Find jump list")
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "qf",
+    callback = function(ev)
+      vim.keymap.set(
+        "n",
+        "<leader>ff",
+        "<cmd>FzfLua quickfix<cr>",
+        { buffer = ev.buf, desc = "Search quickfix" }
+      )
+    end,
+  })
 end
 
 return {

65ab866a753145adb25e6267f309a1d98590d0b5
Hao Chen <chenh1024@gmail.com>
2024-05-08 10:27:00 -0700

    [nvim] move theme configs to themes.lua
    Signed-off-by: Hao Chen <chenh1024@gmail.com>

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -70,61 +70,12 @@ local lualine_opts = {
     },
   },
 }
+
 local function setup_indent_blankline(_, _)
   require("ibl").setup {
     indent = { char = "▏" },
     scope = { enabled = true },
   }
-end
-
-local function setup_onedarkpro(_, _)
-  local color = require("onedarkpro.helpers")
-  local colors = {
-    red = '#ff5c57',
-    green = '#5af78e',
-    yellow = '#f3f99d',
-    blue = '#57c7ff',
-    magenta = '#ff6ac1',
-    cyan = '#9aedfe',
-    purple = color.lighten("purple", 15, "onedark"),
-    orange = color.brighten("orange", 15, "onedark"),
-    comment = color.lighten("comment", 5, "onedark"),
-    fg = color.brighten("fg", 5, "onedark"),
-    light_red = color.lighten("red", 15, "onedark"),
-  }
-
-  local highlights = {
-    Identifier = { fg = "${cyan}", extend = true },
-    ["@variable"] = { link = "Identifier" },
-    ["@variable.parameter"] = { link = "Identifier" },
-    ["@variable.member"] = { link = "Identifier" },
-    ["@odp.interpolation.python"] = { link = "Identifier" }, -- Variables in f-strings.
-    ["@property"] = { fg = "${cyan}", extend = true },
-    String = { fg = "${yellow}", extend = true },
-    pythonString = { link = "String", extend = true },
-    Character = { fg = "${yellow}", extend = true },
-    ["@string"] = { fg = "${yellow}", extend = true },
-    Constant = { fg = "${green}", extend = true },
-    ["@constant"] = { fg = "${green}", extend = true },
-    ["@constant.builtin"] = { link = "Constant" },
-    SpellBad = { undercurl = true, sp = "${red}" },
-    DiagnosticUnderlineError = { undercurl = true, sp = "${red}" },
-  }
-
-  local styles = {
-    comments = "italic",
-  }
-
-  require("onedarkpro").setup({
-    colors = colors,
-    highlights = highlights,
-    styles = styles,
-    options = {
-      cursorline = true,
-      highlight_inactive_windows = false,
-    }
-  })
-  vim.cmd [[colorscheme onedark]]
 end
 
 return {
@@ -194,11 +145,6 @@ return {
     cmd = {
       'SymbolsOutline',
     },
-  },
-  {
-    "olimorris/onedarkpro.nvim",
-    priority = 1000,
-    config = setup_onedarkpro,
   },
   {
     "stevearc/dressing.nvim",

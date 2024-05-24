eaf9c83fb4a2c3811ca3e282f3c08f70517c16df
Hao Chen <chenh1024@gmail.com>
2024-05-23 23:39:17 -0700

    [nvim] add CopilotChat.nvim

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,24 +1,35 @@
-return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  event = "InsertEnter",
-  keys = {
-    {
-      "<leader>cc",
-      function() require("copilot.suggestion").toggle_auto_trigger() end,
-      desc = "Toggle Copilot auto trigger",
+local copilot_keys = {
+  {
+    "<leader>cc",
+    function() require("copilot.suggestion").toggle_auto_trigger() end,
+    desc = "Toggle Copilot auto trigger",
+  },
+}
+
+local copilot_opts = {
+  suggestion = {
+    keymap = {
+      accept = false,
+      next = "<c-_>",
+      prev = false,
     },
   },
-  opts = {
-    suggestion = {
-      keymap = {
-        accept = false,
-        next = "<c-_>",
-        prev = false,
-      },
-    },
-    filetypes = {
-      gitcommit = true,
-    }
+  filetypes = {
+    gitcommit = true,
+  }
+}
+
+return {
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    keys = copilot_keys,
+    opts = copilot_opts,
+  },
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    opts = {},
   },
 }

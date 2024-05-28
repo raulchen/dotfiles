2e7846477cd08deaad57dad227933d2dec85fd9e
Hao Chen <chenh1024@gmail.com>
2024-05-28 13:25:59 -0700

    [nvim] remove nvim-treesitter-textobjects

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -18,60 +18,6 @@ local function setup_treesitter(_, _)
       "yaml",
     },
   }
-  opts.textobjects = {
-    select = {
-      enable = true,
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
-      keymaps = {
-        ["ac"] = { query = "@class.outer", desc = "class outer" },
-        ["ic"] = { query = "@class.inner", desc = "class inner" },
-        ["af"] = { query = "@function.outer", desc = "function outer" },
-        ["if"] = { query = "@function.inner", desc = "function inner" },
-        ["aa"] = { query = "@parameter.outer", desc = "parameter outer" },
-        ["ia"] = { query = "@parameter.inner", desc = "parameter inner" },
-        ["al"] = { query = "@loop.outer", desc = "loop outer" },
-        ["il"] = { query = "@loop.inner", desc = "loop inner" },
-        ["ad"] = { query = "@conditional.outer", desc = "condition outer" },
-        ["id"] = { query = "@conditional.inner", desc = "condition inner" },
-        ["i="] = { query = "@assignment.inner", desc = "assignment outer" },
-        ["a="] = { query = "@assignment.outer", desc = "assignment inner" },
-        ["a/"] = { query = "@comment.outer", desc = "comment outer" },
-        ["i/"] = { query = "@comment.inner", desc = "comment inner" },
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        ["]f"] = { query = "@function.outer", desc = "Next function start" },
-        ["]c"] = { query = "@class.outer", desc = "Next class start" },
-        ["]a"] = { query = "@parameter.outer", desc = "Next parameter start" },
-      },
-      goto_next_end = {
-        ["]F"] = { query = "@function.outer", desc = "Next function end" },
-        ["]C"] = { query = "@class.outer", desc = "Next class end" },
-      },
-      goto_previous_start = {
-        ["[f"] = { query = "@function.outer", desc = "Previous function start" },
-        ["[c"] = { query = "@class.outer", desc = "Previous class start" },
-        ["[a"] = { query = "@parameter.outer", desc = "Previous parameter start" },
-      },
-      goto_previous_end = {
-        ["[F"] = { query = "@function.outer", desc = "Previous function end" },
-        ["[C"] = { query = "@class.outer", desc = "Previous class end" },
-      },
-      -- Below will go to either the start or the end, whichever is closer.
-      goto_next = {
-        ["]d"] = { query = "@conditional.outer", desc = "Next condition start/end" },
-        ["]l"] = { query = "@loop.outer", desc = "Next loop start/end" },
-      },
-      goto_previous = {
-        ["[d"] = { query = "@conditional.outer", desc = "Previous condition start/end" },
-        ["[l"] = { query = "@loop.outer", desc = "Previous loop start/end" },
-      }
-    },
-  }
   require("nvim-treesitter.configs").setup(opts)
   -- Tree-sitter based folding.
   vim.cmd [[
@@ -101,7 +47,6 @@ return {
     build = ":TSUpdate",
     config = setup_treesitter,
     dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
       {
         "nvim-treesitter/nvim-treesitter-context",
         config = setup_treesitter_context,

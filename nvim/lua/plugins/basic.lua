9ae98c705c4684e57f918beca50283f6f1b0f7ee
Hao Chen <chenh1024@gmail.com>
2024-05-29 20:16:15 -0700

    [nvim] minor fix for load_session

--- nvim/lua/plugins/basic.lua
+++ nvim/lua/plugins/basic.lua
@@ -13,7 +13,7 @@ return {
       })
       local keymap = vim.keymap.set
       keymap('n', '<leader>sl', function() session_manager.load_current_dir_session() end, { desc = "Load CWD session" })
-      keymap('n', '<leader>sL', function() session_manager.load_session() end, { desc = "Load session" })
+      keymap('n', '<leader>sL', function() session_manager.load_session(false) end, { desc = "Load session" })
       keymap('n', '<leader>sr', function() session_manager.load_last_session() end, { desc = "Load most recent session" })
       keymap('n', '<leader>sd', function() session_manager.delete_current_dir__session() end,
         { desc = "Delete CWD session" })

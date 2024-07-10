9bb396d5fae729c765f70252f660f6f3ca610eeb
Hao Chen <chenh1024@gmail.com>
2024-07-09 18:18:09 -0700

    Revert "[nvim] add completion for fzf_search"
    This reverts commit 07b6b9fde6be63384eaf1b5b11b9030bef3023d5.

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -92,7 +92,6 @@ local function fzf_search(default_query, default_cwd)
   vim.ui.input({
       prompt = "Search query: ",
       default = default_query,
-      completion = "dir",
     },
     function(query)
       if not query then

6683c4384b90f1bb457c21d00a3974d77110e66b
Hao Chen <chenh1024@gmail.com>
2024-07-16 10:45:35 -0700

    [nvim] lazy load vim plugins

--- nvim/lua/plugins/vim.lua
+++ nvim/lua/plugins/vim.lua
@@ -1,7 +1,10 @@
 local vim_plugins = {}
 
 for _, v in pairs(vim.g.vim_plugins) do
-  local plugin = { v, }
+  local plugin = {
+    v,
+    event = "VeryLazy",
+  }
   table.insert(vim_plugins, plugin)
 end
 

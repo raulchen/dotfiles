8b13c838fa2762aac3bdb55ca19c28f4ce23717d
Hao Chen <chenh1024@gmail.com>
2025-02-08 23:03:35 -0800

    [nvim] map <esc> to clear hlsearch

--- nvim/lua/base/keymaps.lua
+++ nvim/lua/base/keymaps.lua
@@ -1,6 +1,6 @@
-local keymap = vim.keymap.set
+local map = vim.keymap.set
 
-keymap({ 'i', 's' }, '<Tab>', function()
+map({ 'i', 's' }, '<Tab>', function()
   if require("copilot.suggestion").is_visible() then
     require("copilot.suggestion").accept()
   elseif require("luasnip").expand_or_jumpable() then
@@ -10,10 +10,15 @@ keymap({ 'i', 's' }, '<Tab>', function()
   end
 end, { desc = "Super Tab" })
 
-keymap({ 'i', 's' }, '<S-Tab>', function()
+map({ 'i', 's' }, '<S-Tab>', function()
   if require("luasnip").jumpable(-1) then
     require("luasnip").jump(-1)
   else
     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<S-Tab>", true, false, true), "n", false)
   end
 end, { desc = "Super S-Tab" })
+
+map({ "i", "n", "s" }, "<esc>", function()
+  vim.cmd("noh")
+  return "<esc>"
+end, { expr = true, desc = "Escape and clear hlsearch" })

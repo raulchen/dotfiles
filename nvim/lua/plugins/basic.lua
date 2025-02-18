b2d1fef430ca35946daa27b38617bb1d04998963
Hao Chen <chenh1024@gmail.com>
2025-02-17 19:36:09 -0800

    [nvim] lazy load neovim-session-manager

--- nvim/lua/plugins/basic.lua
+++ nvim/lua/plugins/basic.lua
@@ -1,23 +1,28 @@
+local session_manaer_keys = {
+  { "<leader>sl", function() require("session_manager").load_current_dir_session() end, desc = "Load CWD session" },
+  { "<leader>sL", function() require("session_manager").load_session(false) end, desc = "Load session" },
+  { "<leader>sr", function() require("session_manager").load_last_session() end, desc = "Load most recent session" },
+  { "<leader>sd", function() require("session_manager").delete_current_dir_session() end, desc = "Delete CWD session" },
+  { "<leader>sD", function() require("session_manager").delete_session() end, desc = "Delete session" },
+}
+
+local function session_manager_opts()
+  local config = require('session_manager.config')
+  return {
+    autoload_mode = config.AutoloadMode.Disabled,
+    autosave_ignore_dirs = { "~" },
+  }
+end
+
 return {
   {
     "nvim-lua/plenary.nvim",
   },
   {
     "Shatur/neovim-session-manager",
-    config = function()
-      local config = require('session_manager.config')
-      local session_manager = require("session_manager")
-      session_manager.setup({
-        autoload_mode = config.AutoloadMode.Disabled,
-        autosave_ignore_dirs = { "~" },
-      })
-      local keymap = vim.keymap.set
-      keymap('n', '<leader>sl', function() session_manager.load_current_dir_session() end, { desc = "Load CWD session" })
-      keymap('n', '<leader>sL', function() session_manager.load_session(false) end, { desc = "Load session" })
-      keymap('n', '<leader>sr', function() session_manager.load_last_session() end, { desc = "Load most recent session" })
-      keymap('n', '<leader>sd', function() session_manager.delete_current_dir__session() end,
-        { desc = "Delete CWD session" })
-      keymap('n', '<leader>sD', function() session_manager.delete_session() end, { desc = "Delete session" })
-    end,
+    event = "VeryLazy",
+    commands = { "SessionManager" },
+    keys = session_manaer_keys,
+    opts = session_manager_opts,
   },
 }

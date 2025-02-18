563334a56cc53849efe144ba9f18e307827981b6
Hao Chen <chenh1024@gmail.com>
2025-02-17 22:59:26 -0800

    [nvim] refine gitsigns keymaps

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -18,31 +18,37 @@ local function setup_gitsigns()
       end
 
       -- Navigation
-      map('n', ']g', function()
-        if vim.wo.diff then return ']g' end
-        vim.schedule(function() gs.next_hunk() end)
-        return '<Ignore>'
-      end, "Next git hunk", { expr = true })
+      map('n', ']c', function()
+        if vim.wo.diff then
+          vim.cmd.normal({ ']c', bang = true })
+        else
+          gs.nav_hunk('next')
+        end
+      end, "Next change/hunk", { expr = true })
 
-      map('n', '[g', function()
-        if vim.wo.diff then return '[g' end
-        vim.schedule(function() gs.prev_hunk() end)
-        return '<Ignore>'
-      end, "Previous git hunk", { expr = true })
+      map('n', '[c', function()
+        if vim.wo.diff then
+          vim.cmd.normal({ '[c', bang = true })
+        else
+          gs.nav_hunk('prev')
+        end
+      end, "Previous change/hunk", { expr = true })
 
       -- Actions
       map('n', '<leader>gs', gs.stage_hunk, "Stage hunk")
+      map('n', '<leader>gu', gs.undo_stage_hunk, "Undo stage hunk")
       map('n', '<leader>gr', gs.reset_hunk, "Reset hunk")
       map('v', '<leader>gs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end, "Stage hunk")
       map('v', '<leader>gr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end, "Reset hunk")
       map('n', '<leader>gS', gs.stage_buffer, "Stage buffer")
-      map('n', '<leader>gu', gs.undo_stage_hunk, "Undo stage hunk")
       map('n', '<leader>gR', gs.reset_buffer, "Reset buffer")
       map('n', '<leader>gp', gs.preview_hunk, "Preview hunk")
       map('n', '<leader>gb', function() gs.blame_line { full = true } end, "Blame line")
+      map('n', '<leader>gq', gs.setqflist, "Show buffer hunks in quickfix")
+      map('n', '<leader>gQ', function() gs.setqflist('all') end, "Show all hunks in quickfix")
 
       -- Text object
-      map({ 'o', 'x' }, 'ig', ':<C-U>Gitsigns select_hunk<CR>', "Git hunk")
+      map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', "Git hunk")
     end
   }
 end

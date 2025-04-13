830798d617037cf31190eec6a3fee7591f496f52
Hao Chen <chenh1024@gmail.com>
2025-04-12 22:18:05 -0700

    [nvim] add blink.cmp source for avante

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -62,7 +62,16 @@ local blink_opts = {
     }
   },
   sources = {
-    default = { 'lsp', 'path', 'snippets', 'buffer', 'spell' },
+    default = {
+      'lsp',
+      'path',
+      'snippets',
+      'buffer',
+      'spell',
+      "avante_commands",
+      "avante_mentions",
+      "avante_files",
+    },
     providers = {
       buffer = {
         opts = {
@@ -87,7 +96,25 @@ local blink_opts = {
         name = 'Spell',
         module = 'blink-cmp-spell',
         score_offset = -10,
-      }
+      },
+      avante_commands = {
+        name = "avante_commands",
+        module = "blink.compat.source",
+        score_offset = 90,
+        opts = {},
+      },
+      avante_files = {
+        name = "avante_files",
+        module = "blink.compat.source",
+        score_offset = 100,
+        opts = {},
+      },
+      avante_mentions = {
+        name = "avante_mentions",
+        module = "blink.compat.source",
+        score_offset = 1000,
+        opts = {},
+      },
     }
   },
   fuzzy = { implementation = "prefer_rust_with_warning" }
@@ -106,6 +133,19 @@ return {
     dependencies = {
       'rafamadriz/friendly-snippets',
       'ribru17/blink-cmp-spell',
+      {
+        'saghen/blink.compat',
+        version = '*',
+        lazy = true,
+        opts = {},
+        config = function()
+          -- monkeypatch cmp.ConfirmBehavior for Avante
+          require("cmp").ConfirmBehavior = {
+            Insert = "insert",
+            Replace = "replace",
+          }
+        end,
+      },
     },
   },
 }

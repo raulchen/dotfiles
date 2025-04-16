e24fc030c10b40099ce498050fe805fe90bd52b3
Hao Chen <chenh1024@gmail.com>
2025-04-15 20:38:56 -0700

    [nvim] make n/N direction consistent regardless of / or ? search

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -63,6 +63,14 @@ map('v', '<c-k>', ":m '<-2<cr>gv=gv", { desc = 'Move line(s) up' })
 -- System clipboard
 map({ 'n', 'v' }, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
 
+-- Make n/N direction consistent regardless of / or ? search
+map("n", "n", "'Nn'[v:searchforward].'zv'", { expr = true, desc = "Next Search Result" })
+map("x", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next Search Result" })
+map("n", "N", "'nN'[v:searchforward].'zv'", { expr = true, desc = "Prev Search Result" })
+map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev Search Result" })
+
 -- Command line mappings
 -- Ctrl-Y to edit command line
 vim.cmd([[set cedit=\<C-Y>]])

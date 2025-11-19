a7ae27bacd9c5ef3109927e268a47c95db8f72d6
Hao Chen <chenh1024@gmail.com>
2025-11-19 09:35:22 -0800

    [nvim] Remove redundant buffer keymaps: ]b and [b

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -9,9 +9,7 @@ map('n', '<leader>be', '<cmd>enew<cr>', { desc = 'Create new buffer' })
 map('n', '<leader>x', '<cmd>bd<cr>', { desc = 'Delete current buffer' })
 map('n', '<leader>bw', '<cmd>w<cr>', { desc = 'Save current buffer' })
 map({ 'n', 'x' }, '<tab>', '<cmd>bn<cr>', { desc = 'Next buffer' })
-map({ 'n', 'x' }, ']b', '<cmd>bn<cr>', { desc = 'Next buffer' })
 map({ 'n', 'x' }, '<s-tab>', '<cmd>bp<cr>', { desc = 'Previous buffer' })
-map({ 'n', 'x' }, '[b', '<cmd>bp<cr>', { desc = 'Previous buffer' })
 map('n', '<leader>bl', '<c-^>', { desc = 'Switch to last buffer' })
 
 -- Tab management

8e9a6841545881ccb95b8357049270d06e59d574
Hao Chen <chenh1024@gmail.com>
2025-12-05 14:22:29 -0800

    [nvim] add mini.ai for enhanced text objects

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -117,6 +117,50 @@ local mini_surround = {
   }
 }
 
+local mini_ai = {
+  "echasnovski/mini.ai",
+  version = "*",
+  event = "VeryLazy",
+  opts = function()
+    local ai = require("mini.ai")
+    return {
+      n_lines = 500,
+      custom_textobjects = {
+        o = ai.gen_spec.treesitter({ -- code block
+          a = { "@block.outer", "@conditional.outer", "@loop.outer" },
+          i = { "@block.inner", "@conditional.inner", "@loop.inner" },
+        }),
+        f = ai.gen_spec.treesitter({ a = "@function.outer", i = "@function.inner" }), -- function
+        c = ai.gen_spec.treesitter({ a = "@class.outer", i = "@class.inner" }), -- class
+        t = { "<([%p%w]-)%f[^<%w][^<>]->.-</%1>", "^<.->().*()</[^/]->$" }, -- tags
+        d = { "%f[%d]%d+" }, -- digits
+        e = { -- Word with case
+          {
+            "%u[%l%d]+%f[^%l%d]",
+            "%f[%S][%l%d]+%f[^%l%d]",
+            "%f[%P][%l%d]+%f[^%l%d]",
+            "^[%l%d]+%f[^%l%d]",
+          },
+          "^().*()$",
+        },
+        g = function() -- Whole buffer, similar to `gg` and 'G' motion
+          local from = { line = 1, col = 1 }
+          local to = {
+            line = vim.fn.line("$"),
+            col = math.max(vim.fn.getline("$"):len(), 1),
+          }
+          return { from = from, to = to }
+        end,
+        u = ai.gen_spec.function_call(), -- u for "Usage"
+        U = ai.gen_spec.function_call({ name_pattern = "[%w_]" }), -- without dot in function name
+      },
+    }
+  end,
+  config = function(_, opts)
+    require("mini.ai").setup(opts)
+  end,
+}
+
 return {
   vim_tmux_navigator,
   vim_sleuth,
@@ -126,4 +170,5 @@ return {
   mini_cursorword,
   mini_splitjoin,
   mini_surround,
+  mini_ai,
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -21,23 +21,7 @@ local function setup_treesitter(_, _)
   }
   opts.textobjects = {
     select = {
-      enable = true,
-      -- Automatically jump forward to textobj, similar to targets.vim
-      lookahead = true,
-      keymaps = {
-        ["ak"] = { query = "@block.outer", desc = "around block" },
-        ["ik"] = { query = "@block.inner", desc = "inside block" },
-        ["ac"] = { query = "@class.outer", desc = "around class" },
-        ["ic"] = { query = "@class.inner", desc = "inside class" },
-        ["a?"] = { query = "@conditional.outer", desc = "around conditional" },
-        ["i?"] = { query = "@conditional.inner", desc = "inside conditional" },
-        ["af"] = { query = "@function.outer", desc = "around function " },
-        ["if"] = { query = "@function.inner", desc = "inside function " },
-        ["al"] = { query = "@loop.outer", desc = "around loop" },
-        ["il"] = { query = "@loop.inner", desc = "inside loop" },
-        ["aa"] = { query = "@parameter.outer", desc = "around argument" },
-        ["ia"] = { query = "@parameter.inner", desc = "inside argument" },
-      },
+      enable = false,
     },
     move = {
       enable = true,

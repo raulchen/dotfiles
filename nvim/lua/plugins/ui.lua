85182a4b62edbd5a3271295c019c6aa5695a5b9c
Hao Chen <chenh1024@gmail.com>
2026-01-03 10:22:14 -0800

    [nvim] oil: show cwd in winbar

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,5 +1,20 @@
+-- Global function to set winbar for oil buffers
+function _G.get_oil_winbar()
+  local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+  local dir = require("oil").get_current_dir(bufnr)
+  if dir then
+    return vim.fn.fnamemodify(dir, ":~")
+  else
+    -- If there is no current directory (e.g. over ssh), just show the buffer name
+    return vim.api.nvim_buf_get_name(0)
+  end
+end
+
 local function setup_oil()
   require("oil").setup({
+    win_options = {
+      winbar = "%!v:lua.get_oil_winbar()",
+    },
     columns = {
       "icon",
       "permissions",
@@ -33,6 +48,8 @@ local function setup_oil()
       ["g\\"] = "actions.toggle_trash",
     },
     float = {
+      border = "single",
+      get_win_title = function() return "" end,
       max_width = 160,
     },
   })

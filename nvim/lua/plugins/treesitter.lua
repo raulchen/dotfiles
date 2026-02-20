5cc6e19e90f5a572b0cae7293c605326c42ac1d1
Hao Chen <chenh1024@gmail.com>
2026-02-19 21:28:23 -0800

    [nvim] treesitter: align class motions with bracket defaults
    - Use ]] and [[ for class start navigation
    - Add ][ and [] for class end navigation
    - Keep function/block/parameter Treesitter motions unchanged

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -90,23 +90,25 @@ local treesitter_move_mappings = {
     { key = "]k", query = "@block.outer", desc = "Next block start" },
     { key = "]f", query = "@function.outer", desc = "Next function start" },
     { key = "]a", query = "@parameter.inner", desc = "Next parameter start" },
-    { key = "]C", query = "@class.outer", desc = "Next class start" },
+    { key = "]]", query = "@class.outer", desc = "Next class start" },
   },
   goto_next_end = {
     { key = "]K", query = "@block.outer", desc = "Next block end" },
     { key = "]F", query = "@function.outer", desc = "Next function end" },
     { key = "]A", query = "@parameter.inner", desc = "Next parameter end" },
+    { key = "][", query = "@class.outer", desc = "Next class end" },
   },
   goto_previous_start = {
     { key = "[k", query = "@block.outer", desc = "Previous block start" },
     { key = "[f", query = "@function.outer", desc = "Previous function start" },
     { key = "[a", query = "@parameter.inner", desc = "Previous parameter start" },
-    { key = "[C", query = "@class.outer", desc = "Previous class start" },
+    { key = "[[", query = "@class.outer", desc = "Previous class start" },
   },
   goto_previous_end = {
     { key = "[K", query = "@block.outer", desc = "Previous block end" },
     { key = "[F", query = "@function.outer", desc = "Previous function end" },
     { key = "[A", query = "@parameter.inner", desc = "Previous parameter end" },
+    { key = "[]", query = "@class.outer", desc = "Previous class end" },
   },
 }
 
